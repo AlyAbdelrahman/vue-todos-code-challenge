fa2b9dfabc5d19a9e8dfcbeff7b21221155ff4ce
Alyyy <aly.m.abdelrahman@gmail.com>
2019-09-20 15:53:50 +0200

    getting todos from AJAX and adding styling

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
   <div id="app">
     <div class="container">
+      <AddTodo/>
       <Todos/>
     </div>
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import Todos from './components/Todos.vue';
+import AddTodo from './components/AddTodo.vue';
 export default {
   name: 'App',
   components: {
     Todos,
+    AddTodo
   }
 }
 </script>
 
 <style >
-body{
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+body {
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   line-height: 1.6;
   background: #e8f7f0;
 }
-.container{
+.container {
   max-width: 1100px;
   margin: auto;
-  overflow: hidden;
+  overflow: auto;
   padding: 0 2rem;
 }
 </style>
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
 
-const state={};
+const state={
+    todos:[]
+};
 
-const getters={};
+const getters = {
+    allTodos: (state)=> state.todos
+};
 
-const actions={};
+const actions={
+    async fetchTodos({commit}){
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
 
-const mutations={};
+         commit('setTodos',response.data)
+    }
+};
+
+const mutations={
+    setTodos:(state,todos)=>(state.todos=todos)
+};
 
 export default{
     state,

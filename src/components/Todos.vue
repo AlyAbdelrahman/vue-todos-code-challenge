65f2d0f7c5adec9c85ff2626540cdeb1d23c1081
Alyyy <aly.m.abdelrahman@gmail.com>
2019-09-20 22:19:15 +0200

    adding Edit task Compelete/not Compelete

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,33 +1,51 @@
 <template>
   <div>
-    <h3> todos </h3>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"/>= Incomplete
+      </span>
+      <span>
+        <span class="complete-box"/>= Complete
+      </span>
+    </div>
     <div class="todos">
-      <div 
-        v-for="todo in allTodos" 
-        :key="todo.id" 
-        class="todo">{{ todo.title }}
+      <div
+        @dblclick="onDblClick(todo)"
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        :class="{'is-complete':todo.completed}"
+      >
+        {{ todo.title }}
         <i 
           @click="deleteTodo(todo.id)" 
           class="fas fa-trash-alt"/>
       </div>
-    </div>  
-  </div>    
+    </div>
+  </div>
 </template>
 
 <script>
-import {mapGetters,mapActions }  from "vuex";
-
- 
+import { mapGetters, mapActions } from "vuex";
 export default {
-    name:"Todos",
-    methods: {
-        ...mapActions(['fetchTodos','deleteTodo']),
-      
-    },
-    computed: mapGetters(['allTodos']),
-    created(){
-        this.fetchTodos();
+  name: "Todos",
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+      this.updateTodo(updTodo);
     }
+  },
+  computed: mapGetters(["allTodos"]),
+  created() {
+    this.fetchTodos();
+  }
 };
 </script>
 
@@ -46,13 +64,33 @@ export default {
   position: relative;
   cursor: pointer;
 }
-
 i {
   position: absolute;
   bottom: 10px;
   right: 10px;
   color: #fff;
   cursor: pointer;
+}
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+.is-complete {
+  background: #35495e;
+  color: #fff;
 }
 @media (max-width: 500px) {
   .todos {
